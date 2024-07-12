d8294d969496ee0c67d42da001ea5b6db1c5e640
Ivan Porta (GTRekter) <porta.ivan@outlook.com>
2024-07-12 17:59:42 +0900

    Initial commit

---
+++ output.tf
@@ -0,0 +1,14 @@
+output "id" {
+  description = "The ID of Auto Scaling Group."
+  value       = ncloud_auto_scaling_group.auto_scaling_group.id
+}
+
+output "auto_scaling_group_no" {
+  description = "The ID of Auto Scaling Group (It is the same result as id)"
+  value       = ncloud_auto_scaling_group.auto_scaling_group.auto_scaling_group_no
+}
+
+output "server_instance_no_list" {
+  description = "List of server instances belonging to Auto Scaling Group."
+  value       = ncloud_auto_scaling_group.auto_scaling_group.server_instance_no_list
+}
